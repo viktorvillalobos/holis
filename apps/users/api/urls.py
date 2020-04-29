353ac80fa56ea968da66d9b36b9fb9d1a0e35caf
Víctor Villalobos <victor@talana.com>
2020-04-29 11:16:15 -0400

    add jwt

--- apps/users/api/urls.py
+++ apps/users/api/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 from django.urls import include, path
+from rest_framework_simplejwt import views as jwt_views
 
 from apps.users.api import views
 
@@ -11,4 +12,19 @@ else:
 
 router.register("users", views.UserViewSet)
 router.register("notifications", views.NotificationViewSet)
-urlpatterns = router.urls
+
+urlpatterns = [
+    path(
+        'token/',
+        jwt_views.TokenObtainPairView.as_view(),
+        name='token_obtain_pair',
+    ),
+    path(
+        'token/refresh/',
+        jwt_views.TokenRefreshView.as_view(),
+        name='token_refresh',
+    ),
+]
+
+
+urlpatterns += router.urls
--- config/settings/base.py
+++ config/settings/base.py
@@ -306,12 +306,13 @@ STATICFILES_FINDERS += ["compressor.finders.CompressorFinder"]
 # django-rest-framework - https://www.django-rest-framework.org/api-guide/settings/
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 20
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 20
 }
 # Your stuff...
 # ------------------------------------------------------------------------------
