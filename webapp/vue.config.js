690bb28de805680e6bba6bef179502991153bdbc
Víctor Villalobos <victor@talana.com>
2020-04-30 14:54:04 -0400

    fixes

--- webapp/vue.config.js
+++ webapp/vue.config.js
@@ -5,7 +5,7 @@ const webpackFile = process.env.NODE_ENV === 'production' ? '../webapp/webpack-s
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/static/webapp/' : 'http://127.0.0.1:8080/',
-  outputDir: './dist/',
+  outputDir: './assets/webapp/',
 
   chainWebpack: config => {
     config.optimization
