c8b5ebb102d078b6afd007b292fadecb12174a20
Víctor Villalobos <victor@talana.com>
2020-04-30 17:08:16 -0400

    login required

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
 
-
+@login_required
 def webapp(request):
     return render(request, "core/webapp.html")
