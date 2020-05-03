edc848c103286f48f987c0984d3e92fef327d2db
Víctor Villalobos <victor@talana.com>
2020-05-02 21:28:06 -0400

    fix

--- webapp/src/plugins/AxiosDjango.js
+++ webapp/src/plugins/AxiosDjango.js
@@ -3,7 +3,7 @@ import Axios from 'axios'
 const AxiosDjango = {
   /* eslint-disable-next-line */
   install(Vue, options) {
-    if (window.csrf.value) {
+    if (window.csrf && window.csrf.value) {
       console.log(window.csrf.value)
       Axios.defaults.headers.common['X-CSRFTOKEN'] = window.csrf.value;
     }
