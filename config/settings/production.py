7c2c1f91f2e16f7560e0dd914a3a3632bb8c8cd7
Víctor Villalobos <victor@talana.com>
2020-05-02 20:34:21 -0400

    again

--- config/settings/production.py
+++ config/settings/production.py
@@ -214,7 +214,7 @@ sentry_sdk.init(
 WEBPACK_LOADER = {
     "DEFAULT": {
         "CACHE": not DEBUG,
-        "BUNDLE_DIR_NAME": "/static/webapp/",  # must end with slash
+        "BUNDLE_DIR_NAME": "/webapp/",  # must end with slash
         "STATS_FILE": str(ROOT_DIR / "webapp/webpack-stats-prod.json"),
     }
 }
