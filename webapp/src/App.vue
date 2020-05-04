231849481adc1184d10b2cd0b20b20970ab1873f
Víctor Villalobos <victor@talana.com>
2020-05-03 20:46:35 -0400

    fix

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,17 +1,5 @@
 <template>
   <div id="app">
-      <vue-webrtc ref="webrtc"
-          v-show="false"
-          width="100%"
-          :roomId="adslab"
-          :enableAudio="true"
-          :enableVideo="false"
-          v-on:joined-room="logEvent"
-          v-on:left-room="logEvent"
-          v-on:open-room="logEvent"
-          v-on:share-started="logEvent"
-          v-on:share-stopped="logEvent"
-          @error="onError" />
     <router-view></router-view>
   </div>
 </template>
@@ -23,25 +11,8 @@ export default {
     this.$store.dispatch("getList")
   },
   mounted() {
-    this.$refs.webrtc.join()
   },
   methods: {
-   logEvent(event) {
-       console.log('Event : ', event);
-    },
-    onCapture() {
-      this.img = this.$refs.webrtc.capture();
-    },
-    onJoin() {
-      this.$refs.webrtc.join();
-    },
-    onLeave() {
-      this.$refs.webrtc.leave();
-    },
-    onShareScreen() {
-      this.img = this.$refs.webrtc.shareScreen();
-    },
-
   }
 };
 </script>
