6c0eda800bd3cbb38dd3d5fc699a01ff60a07a28
Víctor Villalobos <victor@talana.com>
2020-05-23 20:24:58 -0400

    add onbeforeupload

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -5,9 +5,21 @@
 </template>
 
 <script>
+
+window.onbeforeunload = function(e) {
+    e = e || window.event;
+
+    // For IE and Firefox prior to version 4
+   if (e) {
+        e.returnValue = 'Sure?';
+   }
+   return 'Dialog text here.';
+};
+
 export default {
   name: "App",
-  created() {},
+  created() {
+  },
   mounted() {
   },
   methods: {
