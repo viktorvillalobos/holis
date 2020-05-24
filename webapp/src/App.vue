2a08a7e6dfd052e5096b4d2d673580f49db49743
Víctor Villalobos <victor@talana.com>
2020-05-23 20:44:44 -0400

    fix error lost connection in route change

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,5 +1,20 @@
 <template>
   <div id="app">
+    <webrtc
+      :class="{'aside-expanded' : !float}"
+      ref="webrtc"
+      width="100%"
+      :roomId="room"
+      :enableAudio="enableAudio"
+      :enableVideo="enableVideo"
+      v-on:joined-room="logEvent"
+      v-on:left-room="logEvent"
+      v-on:open-room="logEvent"
+      v-on:share-started="logEvent"
+      v-on:share-stopped="logEvent"
+      @error="onError"
+    />
+
     <router-view></router-view>
   </div>
 </template>
@@ -16,13 +31,50 @@ window.onbeforeunload = function(e) {
    return 'Dialog text here.';
 };
 
+import { mapState } from 'vuex'
+import WebRTC from "@/components/WebRtc"
+
 export default {
   name: "App",
-  created() {
+  components: {
+    webrtc: WebRTC
   },
-  mounted() {
+  computed: {
+    ...mapState({
+      room: state => state.webrtc.room,
+      connected: state => state.webrtc.connected,
+      muteAudio: state => state.webrtc.muteAudio,
+      muteMicro: state => state.webrtc.muteMicro,
+      enableAudio: state => state.webrtc.enableAudio,
+      enableVideo: state => state.webrtc.enableVideo,
+    })
   },
   methods: {
+    logEvent(event) {
+      console.log("Event : ", event);
+    },
+    onError(error, stream) {
+      console.log("On Error Event", error, stream);
+    },
+    onCapture() {
+      this.img = this.$refs.webrtc.capture();
+    },
+    onJoin() {
+      console.log("Join to the connection");
+    },
+    onLeave() {
+      this.$refs.webrtc.leave();
+    },
+    onShareScreen() {
+      this.img = this.$refs.webrtc.shareScreen();
+    }
+  },
+  watch: {
+    connected(value) {
+      console.log('Changing Connected Watcher in Office')
+      if (value) this.$refs.webrtc.join();
+      else this.$refs.webrtc.leave();
+    }
   }
 };
 </script>
