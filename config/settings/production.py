ae37f479a8640d8da5c4d6b249a5c464f0a18932
Víctor Villalobos <victor@talana.com>
2020-05-26 18:37:57 -0400

    fix

--- config/settings/production.py
+++ config/settings/production.py
@@ -15,7 +15,7 @@ from .base import DEBUG, ROOT_DIR, env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["espazum.com"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["holis.chat"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
@@ -100,7 +100,7 @@ TEMPLATES[-1]["OPTIONS"]["loaders"] = [  # type: ignore[index] # noqa F405
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
     "DJANGO_DEFAULT_FROM_EMAIL",
-    default="Espazum Remote Team Tool <noreply@mail.espazum.com>",
+    default="Espazum Remote Team Tool <noreply@mail.holis.chat>",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 # SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
