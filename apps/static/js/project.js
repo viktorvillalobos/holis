2f862fc28bb876583d8ee32ec6ab468ba9cbdc5e
Víctor Villalobos <victor@talana.com>
2020-06-07 17:13:53 -0400

    fixes

--- apps/static/js/project.js
+++ apps/static/js/project.js
@@ -56,4 +56,15 @@ function handleNavMenu() {
             navbar.classList.remove('scrolled')
         }
     })
+
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+          e.preventDefault();
+
+          document.querySelector(this.getAttribute('href')).scrollIntoView({
+              behavior: 'smooth'
+          });
+      });
+  });
 }(document));
