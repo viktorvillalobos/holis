09e74fae4a03d72abda6808d793ce1ef7d63305a
Víctor Villalobos <victor@talana.com>
2020-07-06 22:40:24 -0400

    remove bad import

--- apps/users/api/urls.py
+++ apps/users/api/urls.py
@@ -2,7 +2,6 @@ from apps.users.api import views
 from django.conf import settings
 from django.urls import path
 from rest_framework.routers import DefaultRouter, SimpleRouter
-from rest_framework_simplejwt import views as jwt_views
 
 if settings.DEBUG:
     router = DefaultRouter()
