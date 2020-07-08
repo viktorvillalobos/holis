af77d2ad1e8444e1bcd4ad7f399ccd5eae494f74
Víctor Villalobos <victor@talana.com>
2020-07-07 21:47:38 -0400

    fixes

--- config/routing.py
+++ config/routing.py
@@ -1,13 +1,13 @@
 from apps.core.ws import routing as core_routing
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from apps.core.middleware import TokenAuthMiddleware
+# from apps.core.middleware import TokenAuthMiddleware
 
 websockets = URLRouter([*core_routing.websocket_urlpatterns])
 
 application = ProtocolTypeRouter(
     {
         # (http->django views is added by default)
-        "websocket": TokenAuthMiddleware(websockets)
+        "websocket": AuthMiddlewareStack(websockets)
     }
 )
