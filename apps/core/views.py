5c5de611a012627e34b319f7bc2d651209b75bc3
Víctor Villalobos <victor@talana.com>
2020-07-26 20:09:01 -0400

    redirect app

--- apps/core/views.py
+++ apps/core/views.py
@@ -2,6 +2,7 @@ import logging
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 logger = logging.getLogger(__name__)
 
@@ -13,5 +14,10 @@ def webapp(request):
 
     if not is_subdomain and request.user.is_anonymous:
         return redirect(reverse("web:login"))
+
+    if not is_subdomain and request.user.is_authenticated:
+        scheme_url = request.is_secure() and "https" or "http"
+        url = f"{scheme_url}://{request.user.company.code}.{host}/app/"
+        return HttpResponseRedirect(url)
 
     return render(request, "core/webapp.html")
