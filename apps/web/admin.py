fecab3267fd04e68fd3358ed3854996eec8272eb
Víctor Villalobos <victor@talana.com>
2020-07-31 18:06:44 -0400

    fix

--- apps/web/admin.py
+++ apps/web/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from apps.web.models import Lead
+
 # Register your models here.
+
+
+@admin.register(Lead)
+class LeadAdmin(admin.ModelAdmin):
+    list_display = (
+        "email",
+        "company_name",
+        "company_code",
+        "name",
+        "created",
+        "modified",
+    )
+    search_fields = ("email", "company")
