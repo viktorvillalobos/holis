87b5ba096cd8d789bc96b69722e94555bd8d6868
Víctor Villalobos <victorvillalobos@outlook.com>
2021-01-23 03:22:20 -0300

    chore(holis): upgrade to channel 3

--- apps/chat/ws/routing.py
+++ apps/chat/ws/routing.py
@@ -3,4 +3,4 @@ from django.urls import path
 
 from . import consumers
 
-websocket_urlpatterns = [path("chat/<str:room_name>/", consumers.ChatConsumer)]
+websocket_urlpatterns = [path("chat/<str:room_name>/", consumers.ChatConsumer.as_asgi())]
