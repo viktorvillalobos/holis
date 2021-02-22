23a445fbec20af68d7af3683866489e32bf03fd4
Víktor Villalobos <victorvillalobos@outlook.com>
2021-02-21 21:40:11 -0300

    chore(): Update to python 3.9

--- config/routing.py
+++ config/routing.py
@@ -1,4 +1,5 @@
 from channels.auth import AuthMiddlewareStack
+from channels.http import AsgiHandler
 from channels.routing import ProtocolTypeRouter, URLRouter
 
 from apps.chat.channels import routing as chat_routing
@@ -6,13 +7,14 @@ from apps.core.channels import routing as core_routing
 
 # from apps.core.middleware import TokenAuthMiddleware
 
-websockets = URLRouter(
+websocket_urls = URLRouter(
     [*core_routing.websocket_urlpatterns, *chat_routing.websocket_urlpatterns]
 )
 
 application = ProtocolTypeRouter(
     {
         # (http->django views is added by default)
-        "websocket": AuthMiddlewareStack(websockets)
+        "websocket": AuthMiddlewareStack(websocket_urls),
+        "http": AsgiHandler(),
     }
 )
