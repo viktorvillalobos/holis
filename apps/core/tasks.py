4e6944297483bf9904fb11bca1022c1c1f441f25
Víktor Villalobos <victorvillalobos@outlook.com>
2021-02-28 23:45:51 -0300

    fix import

--- apps/core/tasks.py
+++ apps/core/tasks.py
@@ -4,8 +4,8 @@ from celery.utils.log import get_task_logger
 from channels.layers import get_channel_layer
 
 from apps.core import services as core_services
+from apps.core.channels.utils import force_user_disconect_by_company_and_user_id
 from apps.core.models import Company
-from apps.core.ws.utils import force_user_disconect_by_company_and_user_id
 from apps.users import services as user_services
 
 channel_layer = get_channel_layer()
