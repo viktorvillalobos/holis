43eccfd49e33815d21f0fb20a37b81d9bd109630
Luis Tundisi <luistundisi@gmail.com>
2021-04-26 00:33:37 -0400

    feat(webapp): adding details to screen create project

--- webapp/src/providers/api/projects.js
+++ webapp/src/providers/api/projects.js
@@ -8,23 +8,25 @@ export default {
     var typeNumber = 1
     switch(type) {
       case 'my_projects':
-        typeNumber = 1
+        typeNumber = 3
         break
       case 'my_team':
         typeNumber = 2
         break
       default:
-        typeNumber = 3
+        typeNumber = 1
         break
     }
     return typeNumber
   },
   getProjects(type) {
+    console.log(`${urlBase}/projects/kind/${this.getTypeNumber(type)}`)
     return axios.get(`${urlBase}/projects/kind/${this.getTypeNumber(type)}`)
   },
   createProject (type, payload) {
     console.log("Type",type)
     console.log("Payload",payload)
+    console.log(`${urlBase}/projects/kind/${this.getTypeNumber(type)}`)
     return axios.post(`${urlBase}/projects/kind/${this.getTypeNumber(type)}`, payload)
   },
   addTasksProject (project_uuid, payload) {
