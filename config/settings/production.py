8d105dadd63412148a82d3ec13c0c28d0d4b34b9
Víktor Villalobos <victorvillalobos@outlook.com>
2021-06-06 14:12:33 -0400

    infra(): change config database mistakes

--- config/settings/production.py
+++ config/settings/production.py
@@ -22,8 +22,8 @@ ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["hol.is"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
-DATABASES = {}
-DATABASES["default"] = env.db("DATABASE_URL")  # noqa F405
+# DATABASES = {}
+# DATABASES["default"] = env.db("DATABASE_URL")  # noqa F405
 DATABASES["default"]["ATOMIC_REQUESTS"] = True  # noqa F405
 DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # noqa F405
 
