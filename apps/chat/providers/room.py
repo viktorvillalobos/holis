153006fecaffc2941287880918e2c2a6b88162f5
Víktor Villalobos <victorvillalobos@outlook.com>
2021-07-09 12:43:36 -0400

    feat(chat): refactor provider to make compatible with grupal rooms

--- apps/chat/providers/room.py
+++ apps/chat/providers/room.py
@@ -1,6 +1,6 @@
 from typing import Dict, Iterable, List, Optional, Union
 
-from django.db.models import Count, Exists, Q
+from django.db.models import Case, Count, Exists, Q, Value, When
 from django.db.models.expressions import OuterRef
 from django.db.models.query import QuerySet
 
@@ -11,7 +11,7 @@ from apps.utils.cache import cache
 from apps.utils.html import strip_tags
 from apps.utils.rest_framework.paginators import get_paginated_queryset
 
-from ..models import Room, RoomUserRead
+from ..models import Message, Room, RoomUserRead
 
 
 def get_one_to_one_room_by_members_ids(company_id: int, members_ids: List[int]) -> Room:
@@ -49,11 +49,29 @@ def get_recents_rooms_by_user_id(
     page_size: Optional[int] = 200,
     reverse: Optional[bool] = True,
 ) -> tuple[list[QuerySet], Optional[dict[str, str]], Optional[dict[str, str]]]:
-    is_readed_queryset = RoomUserRead.objects.filter(
+    message_after_last_message = Message.objects.filter(
+        user_id=user_id,
+        company_id=OuterRef("company_id"),
+        room_uuid=OuterRef("uuid"),
+        created__gte=OuterRef("last_message_ts"),
+    )
+
+    readed_after_last_message = RoomUserRead.objects.filter(
         company_id=company_id,
         user_id=user_id,
         room_uuid=OuterRef("uuid"),
-        timestamp__lte=OuterRef("last_message_ts"),
+        timestamp__gte=OuterRef("last_message_ts"),
+    )
+
+    room_messages = Message.objects.filter(
+        company_id=OuterRef("company_id"), room_uuid=OuterRef("uuid")
+    )
+
+    have_unread_messages = Case(
+        When(~Exists(room_messages), then=Value(False)),
+        When(Exists(message_after_last_message), then=Value(False)),
+        When(Exists(readed_after_last_message), then=Value(False)),
+        default=Value(True),
     )
 
     queryset = (
@@ -64,7 +82,7 @@ def get_recents_rooms_by_user_id(
         )
         .exclude(last_message_user_id__isnull=True)
         .prefetch_related("members")
-        .annotate(have_unread_messages=Exists(is_readed_queryset))
+        .annotate(have_unread_messages=have_unread_messages)
         .order_by("have_unread_messages", "-last_message_ts")
     )
 
